0b53f54aa2f65d1571618b7d2be17f2733d551fb
Paul Jaramillo <paulejaramillo@gmail.com>
2025-03-02 22:53:19 -0500

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
-import axios from "./api/axios";
-// import axios from "axios";
+// import axios from "./api/axios";
+import axios from "axios";
 
 function App() {
   const [data, setData] = useState();
   useEffect(() => {
     async function fetchdata() {
-      const response = await axios.get("/");
-      // const response = await axios.get("https://www.wgsitetest.com/");
+      // const response = await axios.get("/");
+      const response = await axios.get("https://www.wgsitetest.com/");
       setData(response.data);
     }
     fetchdata();
