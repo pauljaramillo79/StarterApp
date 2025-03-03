cc2317a126090245063999e52e81dedcd1ed7ee2
Paul Jaramillo <paulejaramillo@gmail.com>
2025-03-02 22:37:02 -0500

    test

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const BASE_URL =
-  process.env.REACT_APP_NODE_ENV === "production" ? "https://www.wgsitetest.com" : "http://localhost:4001";
-// const BASE_URL = "https://www.wgsitetest.com";
+// const BASE_URL =
+//   process.env.REACT_APP_NODE_ENV === "production" ? "https://www.wgsitetest.com" : "http://localhost:4001";
+const BASE_URL = "https://www.wgsitetest.com";
 
 export default axios.create({
   baseURL: BASE_URL,
