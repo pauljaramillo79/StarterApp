b735028208fe81d23a04f1b4fef0e38df38f295c
Paul Jaramillo <paulejaramillo@gmail.com>
2025-03-02 22:08:27 -0500

    test

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins =
   process.env.NODE_ENV === "production"
     ? ["https://www.wgsitetest.com", "https://wgsitetest.com"]
-    : ["http://localhost:4001", "http://localhost:3000"];
+    : ["http://localhost:4001", "http://localhost:3000", "https://www.wgsitetest.com", "https://wgsitetest.com"];
 
 module.exports = allowedOrigins;
