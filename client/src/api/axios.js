c5fefb8459e5f676bfb978f139595da313e9ab61
Paul Jaramillo <paulejaramillo@gmail.com>
2025-03-02 22:39:35 -0500

    run build

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const BASE_URL =
 //   process.env.REACT_APP_NODE_ENV === "production" ? "https://www.wgsitetest.com" : "http://localhost:4001";
-const BASE_URL = "https://www.wgsitetest.com";
+const BASE_URL = "https://wgsitetest.com";
 
 export default axios.create({
   baseURL: BASE_URL,
